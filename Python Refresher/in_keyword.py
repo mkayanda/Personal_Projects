4794ae8cd19342f1fcb626310768234e85114073
Moses Kayanda <55259290+mkayanda@users.noreply.github.com>
2021-04-21 09:45:09 -0700

    Added Data files

---
+++ Python Refresher/in_keyword.py
@@ -0,0 +1,8 @@
+#friends = ["Rolf", "Bob", "Moses"]
+
+#print(friends)
+
+movies_watched = {"The matrix", "Green Book", "Her"}
+user_movie = input("Enter something you've watched recently: ")
+
+print( user_movie in movies_watched) 
